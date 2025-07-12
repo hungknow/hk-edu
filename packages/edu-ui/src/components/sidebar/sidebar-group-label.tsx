46224f98406dc8afe78e70f0df48131ab81dbf03
hungknow <hungbuiknow@gmail.com>
2025-07-12 14:20:49 +0700

    Add the implementation of sidebar

---
+++ packages/edu-ui/src/components/sidebar/sidebar-group-label.tsx
@@ -0,0 +1,20 @@
+import * as React from "react"
+import { cn } from "../../lib/utils"
+import { Slot } from "@radix-ui/react-slot"
+
+function SidebarGroupLabel({ className, asChild = false, ...props }: React.ComponentProps<"div"> & { asChild?: boolean }) {
+  const Comp = asChild ? Slot : "div"
+
+  return (
+    <Comp
+      className={cn(
+        "text-sidebar-foreground/70 ring-sidebar-ring flex h-8 shrink-0 items-center rounded-md px-2 text-xs font-medium outline-hidden transition-[margin,opacity] duration-200 ease-linear focus-visible:ring-2 [&>svg]:size-4 [&>svg]:shrink-0",
+        "group-data-[collapsible=icon]:-mt-8 group-data-[collapsible=icon]:opacity-0",
+        className
+      )}
+      {...props}
+    />
+  )
+}
+
+export { SidebarGroupLabel }
---
+++ packages/edu-ui/src/components/sidebar/sidebar-group.tsx
@@ -0,0 +1,17 @@
+import * as React from "react"
+import { cn } from "../../lib/utils"
+
+function SidebarGroup({ children, className }: React.ComponentProps<"div">) {
+  return (
+    <div
+      className={cn(
+        "relative flex w-full min-w-0 flex-col p-2",
+        className
+      )}
+    >
+      {children}
+    </div>
+  )
+}
+
+export { SidebarGroup }
